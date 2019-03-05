c9b62b9d149de95d87f5ee1c063c5f7be263fa5c
zhilw <zhilw@cenboomh.com>
2019-03-05 10:45:29 +0800

    Issue:优化删除文件夹脚本 作者：吴良志

--- python-base/com/cw/python/文件处理/delete_dir.py
+++ python-base/com/cw/python/文件处理/delete_dir.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import logging
 
 
 # 查找根目录下指定名称的目录及其子目录下所有文件
@@ -10,12 +11,35 @@ def search_del_dir(rootdir, search_dir_name):
             if dir == search_dir_name:
                 path = os.path.join(rootdir, dir)
                 print("deleting dir: " + path)
-                shutil.rmtree(path)
+                try:
+                    # 第二个参数忽略错误
+                    shutil.rmtree(path, ignore_errors=True)
+                    # os_del_file(path)
+                except Exception as e:
+                    print("ERROR：deleting dir: " + path + ", exception:" + repr(e))
+                    logging.exception(e)
             else:
                 search_del_dir(os.path.join(rootdir, dir), search_dir_name)
     else:
         # print(rootdir + " is not a directory")
         return
+
+#使用 os 模块删除文件（最顶层目录未删删掉，待分析原因？）
+def os_del_file(path):
+    for fp in os.listdir(path):
+        #取文件绝对路径
+        path_file = os.path.join(path, fp)
+        # print("path_file: {0}", path_file)
+        # print("length: " + str(len(path_file)) + ", path_file: " + path_file)
+        if len(path_file) > 260:
+            print("file path over 260 character, length: " + str(len(path_file)) + ", path_file: " + path_file)
+            continue
+
+        if os.path.isfile(path_file):
+            os.remove(path_file)
+        else:
+            os_del_file(path_file)
+
 
 
 # 删除target目录
